6f804a79b7713cb810643aa22d5eff756f290d63
shefat26 <shefatmoral@gmail.com>
2024-12-14 12:29:57 -0500

    prep function lab test

---
+++ index.js
@@ -0,0 +1,74 @@
+
+
+//const str1 = "Hello!"
+
+function shout(string){
+
+    return string.toUpperCase();
+
+}
+
+
+
+function whisper(string){
+
+    return string.toLowerCase();
+
+}
+
+
+
+
+function logShout(string){
+
+  
+ 
+
+   console.log(string.toUpperCase());
+
+}
+
+
+
+
+function logWhisper(string){
+
+    
+
+    console.log(string.toLowerCase());
+
+}
+
+
+
+
+
+
+
+function sayHiToHeadphonedRoommate(str){
+
+    if(str === str.toLowerCase()){
+
+        return "I can't hear you!";
+    }
+    else if(str === str.toUpperCase()){
+
+        return "YES INDEED!"
+
+    }
+    else if(str === "Let's have dinner together!"){
+
+        return "I would love to!"
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
